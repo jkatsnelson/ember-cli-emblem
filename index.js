493feda0b80f273dd353780b26a75856a9a12065
John Katsnelson <voltalocke@gmail.com>
2014-09-16 16:26:45 -0700

    attempting a fresh start

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+var path = require('path');
+var EmblemPreprocessor = require('./lib/emblem-preprocessor');
+
+function EmblemAddon(project) {
+  this._project = project;
+  this.name     = 'Ember CLI Emblem Addon';
+}
+
+// EmblemAddon.prototype.blueprintsPath = function() {
+//   return path.join(__dirname, 'blueprints');
+// };
+
+EmblemAddon.prototype.included = function(app) {
+  this.app = app;
+
+  var plugin = new EmblemPreprocessor();
+
+  this.app.registry.add('js', plugin);
+};
+
+// This is just here because it was required in ember-cli v0.0.37.
+// To be removed when compatibility breaks.
+EmblemAddon.prototype.treeFor = function() {};
+
+module.exports = EmblemAddon;
